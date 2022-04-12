4809d22af193184cf85f4a6454b72b475f195d7b
Anis Draganovic <anis00723@gmail.com>
2022-04-12 15:58:00 +0100

    Course Admin Page

--- src/pages/Admin/Courses/index.tsx
+++ src/pages/Admin/Courses/index.tsx
@@ -1,11 +1,178 @@
+import {
+  AnnotationIcon,
+  CalendarIcon,
+  ChevronRightIcon,
+  CurrencyDollarIcon,
+  UsersIcon,
+} from '@heroicons/react/outline';
+import CoursesNavigation from 'components/Courses/CoursesNavigation';
 import AdminLayout from 'components/Layout/AdminLayout';
 import { NextPageWithLayout } from 'pages/_app';
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import StarRatings from 'react-star-ratings';
+import processCouses from 'utils/processCourses';
+import { trpc } from 'utils/trpc';
 
 const CoursesAdminPage: NextPageWithLayout = () => {
+  const [pageIndex, setPageIndex] = useState(0);
+  const coursesQuery = trpc.useInfiniteQuery(['course.all', {}], {
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+  });
+
+  const [firstElementIndex, setFirstElementIndex] = useState(0);
+  const [lastElementIndex, setLastElementIndex] = useState(0);
+
+  const [courses, setCourses] = useState(() =>
+    processCouses(coursesQuery.data?.pages[pageIndex]?.result.items),
+  );
+
+  const [count, setCount] = useState(
+    coursesQuery.data?.pages[pageIndex]?.result.count || 0,
+  );
+
+  const replaceCourses = useCallback((newCourses) => {
+    setCourses(newCourses);
+  }, []);
+
+  useEffect(() => {
+    const courses = processCouses(
+      coursesQuery.data?.pages[pageIndex]?.result.items,
+    );
+    const count = coursesQuery.data?.pages[pageIndex]?.result.count || 0;
+    replaceCourses(courses);
+    setCount(count);
+  }, [coursesQuery.data?.pages, pageIndex, replaceCourses]);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+      /* you can also use 'auto' behaviour
+         in place of 'smooth' */
+    });
+  };
+
+  const handleNextPage = async () => {
+    setPageIndex((pageIndex) => pageIndex + 1);
+    await coursesQuery.fetchNextPage();
+    scrollToTop();
+  };
+
+  const handlePreviousPage = async () => {
+    setPageIndex((pageIndex) => pageIndex - 1);
+    await coursesQuery.fetchPreviousPage();
+    scrollToTop();
+  };
+
+  useEffect(() => {
+    setFirstElementIndex(() => {
+      if (pageIndex === 0) {
+        return 1;
+      }
+      return (
+        pageIndex *
+          // @ts-ignore
+          coursesQuery?.data?.pages[pageIndex - 1]?.result?.items?.length +
+        1
+      );
+    });
+  }, [pageIndex, coursesQuery.data?.pages, courses.length]);
+
+  useEffect(() => {
+    setLastElementIndex(firstElementIndex + courses.length - 1);
+  }, [firstElementIndex, courses]);
+
   return (
     <>
-      <h1>Courses Admin page</h1>
+      <div className="overflow-hidden bg-white shadow sm:rounded-md">
+        <ul role="list" className="divide-y divide-gray-200">
+          {courses.map((course) => (
+            <li key={course.id}>
+              <a href="#" className="flex justify-between hover:bg-gray-50">
+                <div className="grow px-4 py-4 sm:px-6">
+                  <div className="flex items-center justify-between">
+                    <p className="flex  truncate text-sm font-medium text-indigo-600">
+                      {course.name}
+                    </p>
+                    <div className="ml-2 flex flex-shrink-0">
+                      <p className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+                        {course.category.name}
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="mt-2 sm:flex sm:justify-between">
+                    <div className="sm:flex">
+                      <p className="flex items-center text-sm text-gray-500">
+                        <UsersIcon
+                          className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                          aria-hidden="true"
+                        />
+                        {course._count.enrolledUsers}
+                      </p>
+                      <p className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0 sm:ml-6">
+                        <CurrencyDollarIcon
+                          className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                          aria-hidden="true"
+                        />
+                        {course.price}
+                      </p>
+                      <p className="flex items-center text-sm text-gray-500 sm:mt-0 sm:ml-6">
+                        <AnnotationIcon
+                          className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                          aria-hidden="true"
+                        />
+                        {course._count.reviews}
+                      </p>
+                    </div>
+                    <div className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
+                      <div className="mr-4 flex flex-shrink-0">
+                        <StarRatings
+                          rating={course.avgRating}
+                          starDimension="1.25rem"
+                          starSpacing="0.0px"
+                          starRatedColor="rgb(250 204 21)"
+                          starEmptyColor="rgb(229 231 235)"
+                          numberOfStars={5}
+                          svgIconViewBox="1 2 20 20"
+                          svgIconPath="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                          name="rating"
+                        />
+                      </div>
+                      <CalendarIcon
+                        className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                        aria-hidden="true"
+                      />
+                      <p>
+                        Created at{' '}
+                        <time dateTime={course.createdAt.toDateString()}>
+                          {course.createdAt.toDateString()}
+                        </time>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="mr-4 flex w-6 items-center">
+                  <ChevronRightIcon
+                    className="h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </div>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <CoursesNavigation
+        count={count}
+        onNextPage={handleNextPage}
+        onPreviousPage={handlePreviousPage}
+        firstElementIndex={firstElementIndex}
+        lastElementIndex={lastElementIndex}
+        hasNextPage={coursesQuery.data?.pages[pageIndex]?.nextCursor !== null}
+        pageIndex={pageIndex}
+      />
     </>
   );
 };
